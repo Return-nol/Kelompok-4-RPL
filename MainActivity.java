5229c88cffc68cd47f6e7990303541bf4c28c2c5
Agility <32065687+Return-nol@users.noreply.github.com>
2017-12-04 10:47:55 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,58 +1,69 @@
-package id.ac.ui.proyekezprint;
+<?php
+ 
+ServerConfig();
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+$PdfUploadFolder = 'pdf/';
+ 
+$ServerURL = 'http://ezprint.000webhostapp.com/'.$PdfUploadFolder;
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+ 
+ if(isset($_POST['name']) and isset($_FILES["pdf"]["name"])){
 
-public class MainActivity extends AppCompatActivity {
+ $con = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
+ 
+ $PdfName = $_POST['name'];
+ $phone = $_POST['phone'];
+ $line = $_POST['line'];
+ 
+ $PdfInfo = pathinfo($_FILES["pdf"]["name"]);
+ 
+ $PdfFileExtension = $PdfInfo['extension'];
+ 
+ $PdfFileURL = $ServerURL . GenerateFileNameUsingID() . '.' . $PdfFileExtension;
+ 
+ $PdfFileFinalPath = $PdfUploadFolder . GenerateFileNameUsingID() . '.'. $PdfFileExtension;
+ 
+ try{
+ 
+ move_uploaded_file($_FILES["pdf"]["tmp_name"],$PdfFileFinalPath);
+ 
+ $InsertTableSQLQuery = "INSERT INTO pdf (url, name, phone, line, date) VALUES ('$PdfFileURL', '$PdfName', '$phone', '$line', now()) ;";
 
-    Button btn_logout;
-    TextView txt_id, txt_username;
-    String id, username;
-    SharedPreferences sharedpreferences;
+ mysqli_query($con,$InsertTableSQLQuery);
 
-    public static final String TAG_ID = "id";
-    public static final String TAG_USERNAME = "username";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        txt_id = (TextView) findViewById(R.id.txt_id);
-        txt_username = (TextView) findViewById(R.id.txt_username);
-        btn_logout = (Button) findViewById(R.id.btn_logout);
-
-        sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
-
-        id = getIntent().getStringExtra(TAG_ID);
-        username = getIntent().getStringExtra(TAG_USERNAME);
-
-        txt_id.setText("ID : " + id);
-        txt_username.setText("USERNAME : " + username);
-
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                // update login session ke FALSE dan mengosongkan nilai id dan username
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putBoolean(Login.session_status, false);
-                editor.putString(TAG_ID, null);
-                editor.putString(TAG_USERNAME, null);
-                editor.commit();
-
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                finish();
-                startActivity(intent);
-            }
-        });
-
-    }
+ }catch(Exception $e){} 
+ mysqli_close($con);
+ }
 }
+
+function ServerConfig(){
+ 
+define('HostName','localhost');
+define('HostUser','id3508824_user');
+define('HostPass','mahasiswateknik');
+define('DatabaseName','id3508824_login');
+ 
+}
+
+function GenerateFileNameUsingID(){
+ 
+ $con2 = mysqli_connect(HostName,HostUser,HostPass,DatabaseName);
+ 
+ $GenerateFileSQL = "SELECT max(id) as id FROM pdf";
+ 
+ $Holder = mysqli_fetch_array(mysqli_query($con2,$GenerateFileSQL));
+
+ mysqli_close($con2);
+ 
+ if($Holder['id']==null)
+ {
+ return 1;
+ }
+ else
+ {
+ return ++$Holder['id'];
+ }
+}
+
+?>
