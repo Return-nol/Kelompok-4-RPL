2771905b145690ff36eefe2c62626eeb137bda6d
Agility <32065687+Return-nol@users.noreply.github.com>
2017-12-04 10:41:59 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,111 +1,68 @@
 <?php
-/* ===== www.dedykuncoro.com ===== */
-	include 'koneksi.php';
-
-	class usr{}
-
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	$confirm_password = $_POST["confirm_password"];
-
-	if ((empty($username))) {
-		$response = new usr();
-		$response->success = 0;
-		$response->message = "Kolom username tidak boleh kosong";
-		die(json_encode($response));
-	} else if ((empty($password))) {
-		$response = new usr();
-		$response->success = 0;
-		$response->message = "Kolom password tidak boleh kosong";
-		die(json_encode($response));
-	} else if ((empty($confirm_password)) || $password != $confirm_password) {
-		$response = new usr();
-		$response->success = 0;
-		$response->message = "Konfirmasi password tidak sama";
-		die(json_encode($response));
-	} else {
-		if (!empty($username) && $password == $confirm_password){
-			$num_rows = mysql_num_rows(mysql_query("SELECT * FROM users WHERE username='".$username."'"));
-
-			if ($num_rows == 0){
-				$query = mysql_query("INSERT INTO users (id, username, password) VALUES(0,'".$username."','".$password."')");
-
-				if ($query){
-					$response = new usr();
-					$response->success = 1;
-					$response->message = "Register berhasil, silahkan login.";
-					die(json_encode($response));
-
-				} else {
-					$response = new usr();
-					$response->success = 0;
-					$response->message = "Username sudah ada";
-					die(json_encode($response));
-				}
-			} else {
-				$response = new usr();
-				$response->success = 0;
-				$response->message = "Username sudah ada";
-				die(json_encode($response));
-			}
-		}
-	}
-
-	mysql_close();
-
-
 	/* ========= KALAU PAKAI MYSQLI YANG ATAS SEMUA DI REMARK, TERUS YANG INI DI UNREMARK ======== */
-	// include_once "koneksi.php";
+	 include_once "koneksi.php";
 
-	// class usr{}
+	 class usr{}
 
-	// $username = $_POST["username"];
-	// $password = $_POST["password"];
-	// $confirm_password = $_POST["confirm_password"];
+	 $username = $_POST["username"];
+	 $password = $_POST["password"];
+	 $confirm_password = $_POST["confirm_password"];
+	 $phone = $_POST["phone"];
+	 $line = $_POST["line"];
 
-	// if ((empty($username))) {
-	// 	$response = new usr();
-	// 	$response->success = 0;
-	// 	$response->message = "Kolom username tidak boleh kosong";
-	// 	die(json_encode($response));
-	// } else if ((empty($password))) {
-	// 	$response = new usr();
-	// 	$response->success = 0;
-	// 	$response->message = "Kolom password tidak boleh kosong";
-	// 	die(json_encode($response));
-	// } else if ((empty($confirm_password)) || $password != $confirm_password) {
-	// 	$response = new usr();
-	// 	$response->success = 0;
-	// 	$response->message = "Konfirmasi password tidak sama";
-	// 	die(json_encode($response));
-	// } else {
-		// if (!empty($username) && $password == $confirm_password){
-		// 	$num_rows = mysqli_num_rows(mysqli_query($con, "SELECT * FROM users WHERE username='".$username."'"));
+	 if ((empty($username))) {
+	 	$response = new usr();
+	 	$response->success = 0;
+	 	$response->message = "Kolom username tidak boleh kosong";
+	 	die(json_encode($response));
+	 } else if ((empty($password))) {
+	 	$response = new usr();
+	 	$response->success = 0;
+	 	$response->message = "Kolom password tidak boleh kosong";
+	 	die(json_encode($response));
+	 } else if ((empty($phone))) {
+	 	$response = new usr();
+	 	$response->success = 0;
+	 	$response->message = "Kolom phone number tidak boleh kosong";
+	 	die(json_encode($response));
+	 } else if ((empty($line))) {
+	 	$response = new usr();
+	 	$response->success = 0;
+	 	$response->message = "Kolom line id tidak boleh kosong";
+	 	die(json_encode($response));
+	 } else if ((empty($confirm_password)) || $password != $confirm_password) {
+	 	$response = new usr();
+	 	$response->success = 0;
+	 	$response->message = "Konfirmasi password tidak sama";
+	 	die(json_encode($response));
+	 } else {
+	 if (!empty($username) && $password == $confirm_password){
+		 	$num_rows = mysqli_num_rows(mysqli_query($connect, "SELECT * FROM login WHERE username='".$username."'"));
 
-		// 	if ($num_rows == 0){
-		// 		$query = mysqli_query($con, "INSERT INTO users (id, username, password) VALUES(0,'".$username."','".$password."')");
+		 	if ($num_rows == 0){
+		 		$query = mysqli_query($connect, "INSERT INTO login (username, password, phone, line) VALUES('".$username."','".$password."', '".$phone."', '".$line."')");
 
-		// 		if ($query){
-		// 			$response = new usr();
-		// 			$response->success = 1;
-		// 			$response->message = "Register berhasil, silahkan login.";
-		// 			die(json_encode($response));
+		 		if ($query){
+		 			$response = new usr();
+		 			$response->success = 1;
+		 			$response->message = "Register berhasil, silahkan login.";
+		 			die(json_encode($response));
 
-		// 		} else {
-		// 			$response = new usr();
-		// 			$response->success = 0;
-		// 			$response->message = "Username sudah ada";
-		// 			die(json_encode($response));
-		// 		}
-		// 	} else {
-		// 		$response = new usr();
-		// 		$response->success = 0;
-		// 		$response->message = "Username sudah ada";
-		// 		die(json_encode($response));
-		// 	}
-		// }
-	// }
+		 		} else {
+		 			$response = new usr();
+		 			$response->success = 0;
+		 			$response->message = "Gagal memasukkan data";
+		 			die(json_encode($response));
+		 		}
+		 	} else {
+		 		$response = new usr();
+		 		$response->success = 0;
+		 		$response->message = "Username sudah ada";
+		 		die(json_encode($response));
+		 	}
+		 }
+	 }
 
-	// mysqli_close($con);
+	 mysqli_close($connect);
 
 ?>
